e9b4c4f194ddab71a072fd6937183c6cca99f3f1
SavvasMichael <michaelsavvas@ymail.com>
2015-03-30 15:34:14 +0100

    Continuing Shafting process

--- src/main/java/org/savvas/shafted/domain/UserRepository.java
+++ src/main/java/org/savvas/shafted/domain/UserRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 @Repository
 public interface UserRepository extends CrudRepository<ShaftUser, Long> {
     List<ShaftUser> findByEmail(String email);
-
     ShaftUser findByUuid(String uuid);
 }
--- src/main/java/org/savvas/shafted/service/GroupService.java
+++ src/main/java/org/savvas/shafted/service/GroupService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class GroupService {
     private GroupRepository groupRepository;
 
-    public GroupService() {
+    GroupService() {
     }
 
     @Autowired
