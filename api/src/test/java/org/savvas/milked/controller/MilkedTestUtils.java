da410fa16dbc28645f62033661f1f24c96ff5540
SavvasMichael <michaelsavvas@ymail.com>
2015-04-09 01:01:00 +0100

    All tests now green after initial refactor towards milked

--- api/src/test/java/org/savvas/milked/controller/MilkedTestUtils.java
+++ api/src/test/java/org/savvas/milked/controller/MilkedTestUtils.java
@@ -1,10 +1,50 @@
 package org.savvas.milked.controller;
 
+import org.savvas.milked.controller.request.GroupInviteRequest;
+import org.savvas.milked.controller.request.GroupRequest;
+import org.savvas.milked.controller.request.RegistrationRequest;
+import org.savvas.milked.domain.MilkedUser;
+import org.savvas.milked.domain.MilkingGroup;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 import java.util.UUID;
 
 public class MilkedTestUtils {
 
     public static String randomEmail() {
         return UUID.randomUUID().toString()+"@ymail.com";
+    }
+
+    public static MilkedUser givenTheUserIsRegisteredAndActivated(RestTemplate rest, String baseUrl, String name, String password) {
+        RegistrationRequest milkerRegistrationRequest = new RegistrationRequest(randomEmail(), name, password);
+        ResponseEntity<String> milkerRegistrationResponse = rest.postForEntity(URI.create(baseUrl + "/registration"), milkerRegistrationRequest, String.class);
+        String milkerPath = milkerRegistrationResponse.getHeaders().getFirst("Location");
+        ResponseEntity<MilkedUser> milkerResponse = rest.getForEntity(URI.create(baseUrl + milkerPath), MilkedUser.class);
+        return milkerResponse.getBody();
+    }
+
+    public static MilkingGroup givenTheMilkingGroup(RestTemplate rest, String baseUrl, Long milkedUserId, String groupName) {
+        GroupRequest milkerGroupRequest = new GroupRequest(milkedUserId, groupName);
+        ResponseEntity<String> milkerGroupResponse = rest.postForEntity(URI.create(baseUrl +  "/group"), milkerGroupRequest, String.class);
+        String milkerGroupLocation = milkerGroupResponse.getHeaders().getFirst("Location");
+        String milkerGroupUrl = baseUrl + milkerGroupLocation;
+        return rest.getForEntity((URI.create(milkerGroupUrl)), MilkingGroup.class).getBody();
+    }
+
+    public static void givenTheUserHasJoinedTheGroup(RestTemplate rest, String baseUrl, Long milkedUserId, Long mikedGroupId) {
+        String inviteGroupUserLocation = givenTheUserHasBeenInvitedToTheGroup(rest, baseUrl, milkedUserId, mikedGroupId);
+        givenTheUserHasAcceptedTheInvitationToTheGroup(rest, baseUrl, inviteGroupUserLocation);
+    }
+
+    public static String givenTheUserHasBeenInvitedToTheGroup(RestTemplate rest, String baseUrl, Long milkedUserId, Long milkedGroupId) {
+        GroupInviteRequest groupInviteRequest = new GroupInviteRequest(milkedUserId, milkedGroupId);
+        ResponseEntity<String> inviteGroupUserResponse = rest.postForEntity(URI.create(baseUrl + "/group-user"), groupInviteRequest, String.class);
+        return inviteGroupUserResponse.getHeaders().getFirst("Location");
+    }
+
+    public static void givenTheUserHasAcceptedTheInvitationToTheGroup(RestTemplate rest, String baseUrl, String inviteGroupUserLocation) {
+        rest.postForEntity(URI.create(baseUrl + inviteGroupUserLocation + "/activate"), null, String.class);
     }
 }
--- api/src/test/java/org/savvas/milked/controller/MilkingTransactionControllerIntegrationTest.java
+++ api/src/test/java/org/savvas/milked/controller/MilkingTransactionControllerIntegrationTest.java
@@ -2,15 +2,14 @@ package org.savvas.milked.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.savvas.milked.controller.MilkedTestUtils.randomEmail;
+import static org.savvas.milked.controller.MilkedTestUtils.givenTheMilkingGroup;
+import static org.savvas.milked.controller.MilkedTestUtils.givenTheUserHasJoinedTheGroup;
+import static org.savvas.milked.controller.MilkedTestUtils.givenTheUserIsRegisteredAndActivated;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.savvas.milked.Application;
-import org.savvas.milked.controller.request.GroupRequest;
-import org.savvas.milked.controller.request.GroupInviteRequest;
-import org.savvas.milked.controller.request.RegistrationRequest;
-import org.savvas.milked.controller.request.MilkingRequest;
+import org.savvas.milked.controller.request.MilkingTransactionRequest;
 import org.savvas.milked.domain.MilkingTransaction;
 import org.savvas.milked.domain.MilkingGroup;
 import org.savvas.milked.domain.MilkedUser;
@@ -42,8 +41,8 @@ public class MilkingTransactionControllerIntegrationTest {
         String baseUrl = "http://localhost:" + port;
         String shaftUrl = baseUrl + "/shaft";
         //when
-        MilkingRequest milkingRequest = new MilkingRequest(1L, 2L, 5L, 1);
-        ResponseEntity<String> shaftResponse = rest.postForEntity(URI.create(shaftUrl), milkingRequest, String.class);
+        MilkingTransactionRequest milkingTransactionRequest = new MilkingTransactionRequest(1L, 2L, 5L, 1);
+        ResponseEntity<String> shaftResponse = rest.postForEntity(URI.create(shaftUrl), milkingTransactionRequest, String.class);
         String shaftLocation = shaftResponse.getHeaders().getFirst("Location");
         //then
         assertEquals(201, shaftResponse.getStatusCode().value());
@@ -54,43 +53,20 @@ public class MilkingTransactionControllerIntegrationTest {
     public void createMilkingTransactionReturnsCorrectBodyFieldElements() {
         //given
         String baseUrl = "http://localhost:" + port;
-        String createGroupUserUrl = baseUrl + "/group-user";
-        String registrationUrl = baseUrl + "/registration";
-        String activateGroupUserUrl = "/activate";
-        String createGroupUrl = baseUrl + "/group";
         String shaftUrl = baseUrl + "/shaft";
+
+        MilkedUser milker = givenTheUserIsRegisteredAndActivated(rest, baseUrl, "savvas", "password");
+        MilkedUser milkee = givenTheUserIsRegisteredAndActivated(rest, baseUrl, "michael", "password");
+        MilkingGroup milkerGroup = givenTheMilkingGroup(rest, baseUrl, milker.getId(), "SavvasGroup");
+        givenTheUserHasJoinedTheGroup(rest, baseUrl, milkee.getId(), milkerGroup.getId());
+
         //when
-        RegistrationRequest milkerRegistrationRequest = new RegistrationRequest(randomEmail(), "savvas", "password");
-        RegistrationRequest milkeeRegistrationRequest = new RegistrationRequest(randomEmail(), "michael", "password");
-
-        ResponseEntity<String> milkerRegistrationResponse = rest.postForEntity(URI.create(registrationUrl), milkerRegistrationRequest, String.class);
-        ResponseEntity<String> milkeeRegistrationResponse = rest.postForEntity(URI.create(registrationUrl), milkeeRegistrationRequest, String.class);
-
-        String milkerPath = milkerRegistrationResponse.getHeaders().getFirst("Location");
-        String milkeePath = milkeeRegistrationResponse.getHeaders().getFirst("Location");
-
-        ResponseEntity<MilkedUser> milkerResponse = rest.getForEntity(URI.create(baseUrl + milkerPath), MilkedUser.class);
-        ResponseEntity<MilkedUser> milkeeResponse = rest.getForEntity(URI.create(baseUrl + milkeePath), MilkedUser.class);
-        MilkedUser milker = milkerResponse.getBody();
-        MilkedUser milkee = milkeeResponse.getBody();
-
-        GroupRequest milkerGroupRequest = new GroupRequest(milkerResponse.getBody().getId(), "SavvasGroup");
-        ResponseEntity<String> milkerGroupResponse = rest.postForEntity(URI.create(createGroupUrl), milkerGroupRequest, String.class);
-        String milkerGroupLocation = milkerGroupResponse.getHeaders().getFirst("Location");
-        String milkerGroupUrl = baseUrl + milkerGroupLocation;
-        MilkingGroup milkerGroup = rest.getForEntity((URI.create(milkerGroupUrl)), MilkingGroup.class).getBody();
-
-        GroupInviteRequest groupInviteRequest = new GroupInviteRequest(milkerGroup.getId(), milkee.getId());
-        ResponseEntity<String> inviteGroupUserResponse = rest.postForEntity(URI.create(createGroupUserUrl), groupInviteRequest, String.class);
-        String inviteGroupUserLocation = inviteGroupUserResponse.getHeaders().getFirst("Location");
-        rest.postForEntity(URI.create(baseUrl + inviteGroupUserLocation + activateGroupUserUrl), groupInviteRequest, String.class);
-
-        MilkingRequest milkingRequest = new MilkingRequest(milker.getId(), milkee.getId(), milkerGroup.getId(), 100);
-        ResponseEntity<String> milkingResponse = rest.postForEntity(URI.create(shaftUrl), milkingRequest, String.class);
-        String milkedTransactionLocation = milkingResponse.getHeaders().getFirst("Location");
+        MilkingTransactionRequest milkingTransactionRequest = new MilkingTransactionRequest(milker.getId(), milkee.getId(), milkerGroup.getId(), 100);
+        ResponseEntity<String> milkingTransactionResponse = rest.postForEntity(URI.create(shaftUrl), milkingTransactionRequest, String.class);
+        String milkedTransactionLocation = milkingTransactionResponse.getHeaders().getFirst("Location");
         ResponseEntity<MilkingTransaction> milkedTransactionResponse = rest.getForEntity((URI.create(baseUrl + milkedTransactionLocation)), MilkingTransaction.class);
+
         //then
-        assertEquals(201, milkerGroupResponse.getStatusCode().value());
         assertThat(milkedTransactionLocation).matches("^/shaft/\\d+$");
         MilkingTransaction milkingTransaction = milkedTransactionResponse.getBody();
         assertEquals("Unexpected Group Id", milkerGroup.getId(), milkingTransaction.getGroup().getId());
