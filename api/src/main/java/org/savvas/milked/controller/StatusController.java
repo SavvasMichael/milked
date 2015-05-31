85422fde279686e1eb2f091d1f31775337193ba5
SavvasMichael <michaelsavvas@ymail.com>
2015-05-31 13:52:10 +0100

    status controller

---
+++ api/src/main/java/org/savvas/milked/controller/StatusController.java
@@ -0,0 +1,30 @@
+package org.savvas.milked.controller;
+
+import org.savvas.milked.controller.error.ValidationException;
+import org.savvas.milked.controller.request.LoginRequest;
+import org.savvas.milked.domain.MilkedUser;
+import org.savvas.milked.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class StatusController {
+
+    private final LoginService loginService;
+
+    @Autowired
+    public StatusController(LoginService loginService){
+        this.loginService = loginService;
+    }
+
+    @RequestMapping(value = "/status", method = RequestMethod.POST, produces = "text/plain")
+    public String status(){
+        return "ok";
+    }
+}
